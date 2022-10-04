77e68e7a1aeadee3e2aa94db46766fb46eb4191c
xingyurao <raoxingyu13548287984@outlook.com>
2022-10-04 16:09:11 +0200

    function of PSI

---
+++ Bilds-Creation.py
@@ -0,0 +1,8 @@
+# @Time    : 2022/10/4 16:05
+# @Author  : Xingyu Rao
+# @Email   : x.rao@tu-braunschweig.de
+# @File    : Bilds-Creation.py
+# @Software: PyCharm
+
+# create bild of comparison with different steps
+import matplotlib.pyplot as plt
