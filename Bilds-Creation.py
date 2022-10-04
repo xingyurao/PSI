86531575f2ddab5b3617a935e6866b5b90b77250
xingyurao <raoxingyu13548287984@outlook.com>
2022-10-05 00:22:16 +0200

    function of PSI

--- Bilds-Creation.py
+++ Bilds-Creation.py
@@ -9,12 +9,29 @@ import matplotlib.pyplot as plt
 from n_step_algorithms.n_step_Algorithms import *
 import numpy as np
 
-
 # 3 step PSI
 phase_interval = (0, np.pi * 2)
-get_value, m_std = n_step_single_line(step=3, phase=phase_interval)
+_, m_std_3 = n_step_single_line(step=3, phase=phase_interval)
+_, m_std_4 = n_step_single_line(step=4, phase=phase_interval)
+#_, m_std_5 = n_step_single_line(step=5, phase=phase_interval)
+#_, m_std_8 = n_step_single_line(step=8, phase=phase_interval)
 
-m_std = outlier_delete(m_std)
-plt.figure(1)
-plt.plot(m_std)
-plt.show(block=1)
+m_std_3 = outlier_delete(m_std_3)
+m_std_4 = outlier_delete(m_std_4)
+#m_std_5 = outlier_delete(m_std_5)
+#m_std_8 = outlier_delete(m_std_8)
+
+#%%
+fig = plt.figure()
+plt.style.use('scientific')
+plt.plot(m_std_3, label='3-step')
+plt.plot(m_std_4, label='4-step')
+#plt.plot(m_std_5, label='5-step')
+#plt.plot(m_std_8, label='8-step')
+
+plt.title('comparison with different sampling periods')
+plt.xlabel('startphase')
+plt.ylabel('standard deviation')
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.show(block=True)
