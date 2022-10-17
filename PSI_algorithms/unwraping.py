7f461bf89402cd33d58cdf0b34df8f8502ca2df0
xingyurao <raoxingyu13548287984@outlook.com>
2022-10-17 16:52:32 +0200

    function of PSI

--- PSI_algorithms/unwraping.py
+++ PSI_algorithms/unwraping.py
@@ -3,28 +3,32 @@
 # @Email   : x.rao@tu-braunschweig.de
 # @File    : unwraping.py
 # @Software: PyCharm
-import numpy as np
+
+
 from PSI_algorithms.N_Step import *
 import matplotlib.pyplot as plt
 
-def unwraping(data=np.zeros([720]), lamda=530):
+
+def unwraping(line=np.zeros([720]), lamda=530):
     factor = lamda / (4 * np.pi)
-
-    for i in np.arange(0, np.size(data) - 1, 1):
-        if np.isclose(np.abs(data[i + 1] - data[i]), np.pi * factor, 1.e-1):
-            data[i + 1:]= data[i + 1:] + (data[i] - data[i + 1]) / np.abs(data[i + 1] - data[i]) * np.pi * factor
-
-    return [data]
+    for i in np.arange(0, np.size(line) - 1, 1):
+        if np.isclose(np.abs(line[i + 1] - line[i]), np.pi * factor, 1.e-1):
+            line[i + 1:] = line[i + 1:] + (line[i] - line[i + 1]) / np.abs(line[i + 1] - line[i]) * np.pi * factor
 
 
-data, _ = n_step_single_line(step=3, loop=1)
-data = data[0]
+    return line
+
+
+# %%
+data1, _ = n_step_single_line(step=3, loop=1)
+data_0 = data1[0]
 plt.figure()
-plt.plot(data)
+plt.plot(data_0, 'b-')
 plt.show(block=1)
 
-data_new=unwraping(data)[0]
+new = unwraping(data1[0])
+
 plt.figure()
-plt.plot(data)
-plt.plot(data_new,'k--')
+plt.plot(data_0, 'b-')
+plt.plot(unwraping(data1[0]), 'k--')
 plt.show(block=1)
