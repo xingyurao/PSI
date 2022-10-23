1c3af547b9f3f79522d1a7dc4f5554af14f88bdf
xingyurao <raoxingyu13548287984@outlook.com>
2022-10-24 01:41:52 +0200

    function of PSI

--- Test.py
+++ Test.py
@@ -31,3 +31,39 @@ for theta in np.linspace(0,np.pi*2,1000):
 '''
 
 
+# %% comparison between (N+1/2N) sampling und N+1/2N sampling interferograms
+import matplotlib.pyplot as plt
+from PSI_algorithms.N_plus_M_Step import *
+from PSI_algorithms.N_Step import *
+from PSI_algorithms.Data_Process import *
+
+std_NN = np.array([])
+std_NM = np.array([])
+x=np.arange(4,20,2)
+x=(x+x/2).astype(int)
+for N in np.arange(4, 20, 2):
+    _, std = n_step(step=N + N / 2,position_noise=False,shot_noise=True)
+    std_NN = np.append(std_NN, np.nanmean(std))
+    _, std = N_M_step(step=N, over_sample_points=int(N / 2),position_noise=False,shot_noise=True)
+    std_NM = np.append(std_NM, np.nanmean(std))
+
+plt.style.use('scientific')
+plt.figure()
+factor = 530 / (4 * np.pi)
+plt.loglog(x, std_NN, 'k', label='N sampling interferograms',marker='o',markeredgecolor='k')
+plt.loglog(x, std_NM, 'r', label='N+M sampling interferograms',marker='o',markeredgecolor='r')
+
+plt.xlabel('Number of sampling interferograms')
+plt.ylabel('Standard deviation, nm')
+
+plt.xticks([5,6, 15, 20, 30], ['',r'$6$', r'$15$','', r'$30$'])
+plt.yticks([0.04,0.05,0.06,0.1,0.2,0.3],['',r'$0.05$','',r'$0.1$',r'$0.2$',''])
+plt.xlim(5, 30)
+plt.ylim(0.08,0.3)
+plt.legend(loc='upper right')
+plt.title('Comparison between N und N+M sampling interferograms')
+plt.tight_layout()
+plt.savefig('Images/Shot Noise/comparison sampling sampling interferograms',
+            bbox_inches='tight')
+plt.show(block=1)
+
